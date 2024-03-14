dc5aa0fb83c6b0c58239154c2b67d25f124afd7a
saucepoint <saucepoint@protonmail.com>
2024-03-14 00:46:55 +0000

    dynamic fee example with tests

--- src/DynamicFeeHook.sol
+++ src/DynamicFeeHook.sol
@@ -6,25 +6,14 @@ import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 
 contract DynamicFeeHook is BaseHook {
-    using PoolIdLibrary for PoolKey;
-
-    // NOTE: ---------------------------------------------------------
-    // state variables should typically be unique to a pool
-    // a single hook contract should be able to service multiple pools
-    // ---------------------------------------------------------------
-
-    mapping(PoolId => uint256 count) public beforeSwapCount;
-
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
             beforeInitialize: false,
-            afterInitialize: false,
+            afterInitialize: false, // you can use afterInitialize to set the initial swap fee too
             beforeAddLiquidity: false,
             afterAddLiquidity: false,
             beforeRemoveLiquidity: false,
@@ -36,16 +25,23 @@ contract DynamicFeeHook is BaseHook {
         });
     }
 
-    // -----------------------------------------------
-    // NOTE: see IHooks.sol for function documentation
-    // -----------------------------------------------
+    /// @notice Sets the swap fee for a pool
+    /// @dev Define your own custom logic here!
+    function setDynamicFee(PoolKey calldata key) public {
+        if (block.number % 2 == 0) {
+            poolManager.updateDynamicSwapFee(key, 6900); // 0.69%
+        } else {
+            poolManager.updateDynamicSwapFee(key, 500); // 0.05%
+        }
+    }
 
     function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
         external
         override
         returns (bytes4)
     {
-        beforeSwapCount[key.toId()]++;
+        setDynamicFee(key); // set the swap fee on every swap
+        // TODO: optimization -- only needs to be set top-of-block
         return BaseHook.beforeSwap.selector;
     }
 }
--- test/DynamicFeeHook.t.sol
+++ test/DynamicFeeHook.t.sol
@@ -10,17 +10,18 @@ import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
-import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
 import {Deployers} from "v4-core/test/utils/Deployers.sol";
 import {DynamicFeeHook} from "../src/DynamicFeeHook.sol";
 import {HookMiner} from "./utils/HookMiner.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {SwapFeeLibrary} from "v4-core/src/libraries/SwapFeeLibrary.sol";
 
 contract DynamicFeeHookTest is Test, Deployers {
     using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
+    using FixedPointMathLib for uint256;
 
     DynamicFeeHook hook;
-    PoolId poolId;
 
     function setUp() public {
         // creates the pool manager, utility routers, and test tokens
@@ -28,42 +29,73 @@ contract DynamicFeeHookTest is Test, Deployers {
         Deployers.deployMintAndApprove2Currencies();
 
         // Deploy the hook to an address with the correct flags
-        uint160 flags = uint160(
-            Hooks.BEFORE_SWAP_FLAG
-        );
+        uint160 flags = uint160(Hooks.BEFORE_SWAP_FLAG);
         (address hookAddress, bytes32 salt) =
             HookMiner.find(address(this), flags, type(DynamicFeeHook).creationCode, abi.encode(address(manager)));
         hook = new DynamicFeeHook{salt: salt}(IPoolManager(address(manager)));
         require(address(hook) == hookAddress, "DynamicFeeHookTest: hook address mismatch");
 
-        // Create the pool
-        key = PoolKey(currency0, currency1, 3000, 60, IHooks(address(hook)));
-        poolId = key.toId();
+        // Create the pool with dynamic fees enabled
+        key = PoolKey(currency0, currency1, SwapFeeLibrary.DYNAMIC_FEE_FLAG, 60, IHooks(address(hook)));
         manager.initialize(key, SQRT_RATIO_1_1, ZERO_BYTES);
 
         // Provide liquidity to the pool
-        modifyLiquidityRouter.modifyLiquidity(key, IPoolManager.ModifyLiquidityParams(-60, 60, 10 ether), ZERO_BYTES);
-        modifyLiquidityRouter.modifyLiquidity(key, IPoolManager.ModifyLiquidityParams(-120, 120, 10 ether), ZERO_BYTES);
+        modifyLiquidityRouter.modifyLiquidity(key, IPoolManager.ModifyLiquidityParams(-60, 60, 10000 ether), ZERO_BYTES);
+        modifyLiquidityRouter.modifyLiquidity(
+            key, IPoolManager.ModifyLiquidityParams(-120, 120, 1000 ether), ZERO_BYTES
+        );
         modifyLiquidityRouter.modifyLiquidity(
             key,
-            IPoolManager.ModifyLiquidityParams(TickMath.minUsableTick(60), TickMath.maxUsableTick(60), 10 ether),
+            IPoolManager.ModifyLiquidityParams(TickMath.minUsableTick(60), TickMath.maxUsableTick(60), 10000 ether),
             ZERO_BYTES
         );
     }
 
-    function testCounterHooks() public {
-        // positions were created in setup()
+    function test_dynamic_fee_even_block_number() public {
+        // set block number to 10 (even), making the dynamic fee 0.69%
+        vm.roll(10);
+        assertEq(block.number, 10);
 
-        assertEq(hook.beforeSwapCount(poolId), 0);
+        uint256 balance1Before = currency1.balanceOfSelf();
 
         // Perform a test swap //
         bool zeroForOne = true;
         int256 amountSpecified = 1e18;
-        BalanceDelta swapDelta = swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+        BalanceDelta swapDelta = Deployers.swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
         // ------------------- //
 
-        assertEq(int256(swapDelta.amount0()), amountSpecified);
+        uint256 token1Output = currency1.balanceOfSelf() - balance1Before;
 
-        assertEq(hook.beforeSwapCount(poolId), 1);
+        assertEq(int256(swapDelta.amount0()), amountSpecified);
+        assertEq(int256(swapDelta.amount1()), -int256(token1Output));
+
+        // tokens are trading 1:1, so 1e18 input should produce roughly 0.9931e18 output (0.69% fee)
+        // (fee is taken from the input, which leads to a smaller output)
+        // need to use approx-assertion because tokens are not trading exactly 1:1
+        assertApproxEqAbs(token1Output, uint256(amountSpecified).mulWadDown(0.9931e18), 0.00005e18);
+    }
+
+    function test_dynamic_fee_odd_block_number() public {
+        // set block number to 11 (odd), making the dynamic fee 0.05%
+        vm.roll(11);
+        assertEq(block.number, 11);
+
+        uint256 balance1Before = currency1.balanceOfSelf();
+
+        // Perform a test swap //
+        bool zeroForOne = true;
+        int256 amountSpecified = 1e18;
+        BalanceDelta swapDelta = Deployers.swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+        // ------------------- //
+
+        uint256 token1Output = currency1.balanceOfSelf() - balance1Before;
+
+        assertEq(int256(swapDelta.amount0()), amountSpecified);
+        assertEq(int256(swapDelta.amount1()), -int256(token1Output));
+
+        // tokens are trading 1:1, so 1e18 input should produce roughly 0.9995e18 output (0.05% fee)
+        // (fee is taken from the input, which leads to a smaller output)
+        // need to use approx-assertion because tokens are not trading exactly 1:1
+        assertApproxEqAbs(token1Output, uint256(amountSpecified).mulWadDown(0.9995e18), 0.00005e18);
     }
 }
